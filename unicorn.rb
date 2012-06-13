23f1cc58820aa375bdbd0a433e3736d91087d942
Lucas Nasif <lucas@citrusbyte.com>
2012-06-13 15:18:14 -0300

    Update the unicorn.rb config file.

--- unicorn.rb
+++ unicorn.rb
@@ -1,2 +1,2 @@
-worker_processes 5 # amount of unicorn workers to spin up
-timeout 20         # restarts workers that hang for 20 seconds
+worker_processes 3 # amount of unicorn workers to spin up
+timeout 15         # restarts workers that hang for 20 seconds
