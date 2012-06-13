660c0e003c12850568b81224f6b35195ca597ea5
Lucas Nasif <lucas@citrusbyte.com>
2012-06-13 17:44:19 -0300

    Tweak the unicorn config.

--- unicorn.rb
+++ unicorn.rb
@@ -1,2 +1,3 @@
-worker_processes 2 # amount of unicorn workers to spin up
+worker_processes 4 # amount of unicorn workers to spin up
 timeout 15         # restarts workers that hang for 20 seconds
+preload_app true   # Faster worker spawn times.
